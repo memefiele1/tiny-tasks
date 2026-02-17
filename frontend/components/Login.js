f78d0a3e275796462387a5064d47aec165224454
Kayla Thornton <kaylathornton55@icloud.com>
2026-02-17 10:15:17 -0500

    chore: Login page UI implemented

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -5,10 +5,10 @@ redirects them to the dashboard upon successful login.
  */
 import React, { useState } from "react";
 import {
-  Button,
   KeyboardAvoidingView,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
 
@@ -16,13 +16,11 @@ export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState("");
-  // count how many attempts user makes and lock account after 3 unsuccessful attempts
 
   // Validate user login information
   const validateForm = () => {
     if (!username || !password) {
       setErrors("Incorrect username or password");
-
       return false;
     } else {
       return true;
@@ -40,35 +38,78 @@ export default function Login() {
     } else {
       console.log("Unsuccessful login attempt");
     }
-    // count how many times user attempts to login, lock account after 3 unsuccessful attempts
   };
 
   // TO-DO - add "forgot password" and "sign-up" links to this page (I think these should be buttons on the homescreen)
   return (
     <View>
       <KeyboardAvoidingView>
-        <Text> Username </Text>
-        <TextInput
-          placeholder="Username"
-          value={username}
-          onChangeText={setUsername}
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
+        <View style={{ margin: 10 }}>
+          <Text style={{ fontSize: 24, textAlign: "center" }}>
+            {" "}
+            Tiny Tasks{" "}
+          </Text>
+          <Text style={{ fontSize: 16, textAlign: "center", color: "gray" }}>
+            Task management app for students
+          </Text>
+        </View>
 
-        <Text> Password </Text>
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          autoCorrect={false}
-          autoCapitalize="none"
-          secureTextEntry
-        />
+        <View style={{ gap: 10, marginBottom: 16 }}>
+          {{ errors } ? (
+            <Text style={{ fontSize: 12, color: "red" }}> {errors} </Text>
+          ) : null}
 
-        <Button accessibilityLabel="log in" title="Log In" onPress={onLogin} />
+          <Text style={{ fontSize: 14, fontWeight: "700" }}> Username </Text>
+          <TextInput
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+            autoCorrect={false}
+            autoCapitalize="none"
+            style={{
+              borderWidth: 1,
+              borderRadius: 12,
+              padding: 10,
+              fontSize: 14,
+            }}
+          />
 
-        {{ errors } ? <Text> {errors} </Text> : null}
+          <Text style={{ fontSize: 14, fontWeight: "700" }}> Password </Text>
+          <TextInput
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            autoCorrect={false}
+            autoCapitalize="none"
+            secureTextEntry
+            style={{
+              borderWidth: 1,
+              borderRadius: 12,
+              padding: 10,
+              fontSize: 14,
+            }}
+          />
+        </View>
+
+        <TouchableOpacity
+          accessibilityLabel="log in"
+          onPress={onLogin}
+          style={{
+            flex: 1,
+            padding: 10,
+            borderRadius: 14,
+            borderWidth: 1,
+            backgroundColor: "black",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 18, color: "white" }}> Log In </Text>
+        </TouchableOpacity>
+
+        <Text style={{ fontSize: 16, textAlign: "center", color: "gray" }}>
+          {" "}
+          Don&apos;t have an account? Register Here
+        </Text>
       </KeyboardAvoidingView>
     </View>
   );
