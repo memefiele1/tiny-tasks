cb58d77f6bd548a87e91c78cac1986abd35d220f
Kayla Thornton <kaylathornton@Kaylas-MacBook-Air.local>
2026-03-27 17:51:45 -0400

    clean-up - ForgotPassword file refactored to contain updated UI

--- frontend/app/ForgotPassword.tsx
+++ frontend/app/ForgotPassword.tsx
@@ -3,8 +3,9 @@
     Purpose - Allow users to reset passwords 
  */
 import Button from '@/ui/Button';
+import { SPACING } from '@/ui/CustomStyles';
 import Screen from '@/ui/Screen';
-import { Heading } from '@/ui/Text';
+import { BodyText, Heading } from '@/ui/Text';
 import { useState } from 'react';
 import { TextInput } from 'react-native';
 
@@ -12,11 +13,13 @@ export default function ForgotPassword() {
     // capture password information
     const [ password, setPassword ] = useState("");
     const [ confirmPswd, setConfirmPswd ] = useState("");
+    const [ errors, setErrors ] = useState("");
 
     // send password request to backend
     const handlePasswordReset = () => {
         console.log('Reset Password');
 
+        // send password to backend to validate new password
         setPassword('');
         setConfirmPswd('');
     }
@@ -27,24 +30,44 @@ export default function ForgotPassword() {
             
             <Heading> Password Reset </Heading>
 
-            <TextInput // ask Tiff to update to include autoCapitalize and auoCorrect
+            { errors ? <BodyText> {errors} </BodyText> : null }
+            <TextInput 
                 placeholder='Enter Password'
                 value={password}
                 onChangeText={setPassword}
                 autoCapitalize="none"
                 autoCorrect={false}
                 secureTextEntry
+                style={{
+                    borderWidth: 1,
+                    borderRadius: 12,
+                    padding: 10,
+                    fontSize: 14,
+                    marginBottom: SPACING.lg
+                }}
             />
 
             <TextInput
-                placeholder='Enter Password'
+                placeholder='Confirm Password'
                 value={confirmPswd}
                 onChangeText={setConfirmPswd}
                 autoCapitalize="none"
                 autoCorrect={false}
                 secureTextEntry
+                style={{
+                    borderWidth: 1,
+                    borderRadius: 12,
+                    padding: 10,
+                    fontSize: 14,
+                    marginBottom: SPACING.lg
+                }}
             />
 
+            <BodyText variant='caption'>
+                Passwords must: be 8 characters long (minimum), one numeric value, one
+                uppercase letter, and one lowecase letter
+            </BodyText>
+            
             <Button 
                 label='Submit'
                 onPress={handlePasswordReset}
